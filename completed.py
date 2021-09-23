b0c782ab7f4c4f52632836e438b9e949c09ac5db
tharunptk95 <65007507+tharunptk95@users.noreply.github.com>
2021-09-23 12:01:33 +0530

    Add files via upload

--- completed.py
+++ completed.py
@@ -1,6 +1,15 @@
 import cv2
+import serial # serial communication
+import time  
 
-cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+ser = serial.Serial(port="COM3",baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
+        
+
+
+cascade_classifier = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -12,12 +21,21 @@ while True:
     if(len(detections) > 0):
         (x,y,w,h) = detections[0]
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        # print(1)
+        # if ser.is_open:
+            # time.sleep(1)
+    if(len(detections)>0):
+        time.sleep(1)
+        ser.write("A".encode('utf-8'))
+
+
 
     # for (x,y,w,h) in detections:
     # 	frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
